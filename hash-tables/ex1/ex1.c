914df517eb8d8fdb174392f30cecb41527f1bdee
RachelKolk <rach.m.obrien@gmail.com>
2019-06-14 12:09:52 -0400

    Hash Tables ex1 done - tests passing

--- hash-tables/ex1/ex1.c
+++ hash-tables/ex1/ex1.c
@@ -6,9 +6,37 @@
 Answer *get_indices_of_item_weights(int *weights, int length, int limit)
 {
   HashTable *ht = create_hash_table(16);
-
+  
   /* YOUR CODE HERE */
-
+  Answer *answer = malloc(sizeof(Answer));
+  // put the weight into the hash table as the key, using the insert function
+  // and the value will be their index in the hash table 
+  for (int i = 0; i < length; i++) {
+    hash_table_insert(ht, weights[i], i);
+  }
+  // using the retrieve function we'll try to find the correct weights that add up to the limit
+  for (int i = 0; i < length; i++) {
+    // subtract the limit weight from the weight stored as the key to get the difference
+    // or the key we are looking for
+    // save the retrieved value as an int so we can use it later
+    int retrieved_value = hash_table_retrieve(ht, limit - weights[i]);
+    // check to make sure that the key was found
+    // and that the retrieved_value is not the same as the current i value 
+    if (retrieved_value != -1 && retrieved_value != i) {
+      // check to see which value is bigger to put them in the correct index field
+      if (retrieved_value <= i) {
+        answer->index_1 = i;
+        answer->index_2 = retrieved_value;
+        return answer;
+      } else {
+        answer->index_1 = retrieved_value;
+        answer->index_2 = i;
+        return answer;
+      }
+    }
+  }
+  // use destroy_hash_table function to combat memory leaks
+  destroy_hash_table(ht);
   return NULL;
 }
 
